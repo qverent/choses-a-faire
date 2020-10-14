fd4eb326030e585b15dffd654cf3a0dfb9e03b9c
qverent <erislam@hotmail.ca>
2020-10-13 21:36:46 -0700

    [SETUP] Simplified index.js

--- src/index.js
+++ src/index.js
@@ -2,28 +2,10 @@ require('dotenv').config();
 const { GraphQLServer } = require('graphql-yoga')
 const { PrismaClient } = require('@prisma/client')
 
-const PORT = process.env.PORT || 4000;
-const prisma = new PrismaClient({
-    log: [
-        { emit: 'event', level: 'warn'},
-        { emit: 'event', level: 'info'}
-    ]
-});
+const prisma = new PrismaClient();
 
-prisma.$on('warn', event => {
-    e.timestamp;
-    e.message;
-    e.target;
-    console.log('[DB WARN]', e);
-  });
 
-prisma.$on('info', event => {
-  e.timestamp;
-  e.message;
-  e.target;
-  console.log('[DB INFO]', e);
-});
-
+// GraphQL Server setup
 const Query = require('./graphql/resolvers/Query')
 const Mutation = require('./graphql/resolvers/Mutation')
 const User = require('./graphql/resolvers/User')
@@ -37,7 +19,7 @@ const resolvers = {
 }
 
 const options = {
-    port: PORT,
+    port: process.env.PORT || 4000,
     endpoint: '/graphql',
     playground: '/playground'
 }
@@ -52,6 +34,7 @@ const server = new GraphQLServer({
     }
   }
   })
+
   server.start(options, () => console.log(`Server is running on http://localhost:${PORT}`))    
 
 
